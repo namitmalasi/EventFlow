b1f95391f341d42ab23819f60a9c89e3f4fc67c6
Namit Malasi <namitmalasi@gmail.com>
2024-08-13 13:33:21 +0530

    setting up ant design library

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,15 @@
+import { Button, Input } from "antd";
+import ThemeProvider from "./theme";
 function App() {
   return (
-    <div className="h-screen flex justify-center items-center bg-gray-500">
-      <h1 className="text-white text-3xl">Homepage</h1>
-    </div>
+    <ThemeProvider>
+      <div className="h-screen flex justify-center items-center flex-col p-5 gap-5">
+        <h1 className="text-gray-600 text-3xl">Homepage</h1>
+
+        <Button type="primary">Primary Button</Button>
+        <Input placeholder="enter text" />
+      </div>
+    </ThemeProvider>
   );
 }
 
