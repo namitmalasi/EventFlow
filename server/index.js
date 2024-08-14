571fab5d938676987a7c01df3b5deddd0e14cae1
Namit Malasi <namitmalasi@gmail.com>
2024-08-14 16:49:28 +0530

    MongoDB connection

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const { connectMongoDB } = require("./config/db-config");
 
 const app = express();
+require("dotenv").config();
 
+connectMongoDB();
 const port = process.env.port || 5000;
 
 app.listen(port, () => {
