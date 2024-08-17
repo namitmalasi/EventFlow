8b9c2b754ca4e074a023267723ae08782bf99b2f
Namit Malasi <namitmalasi@gmail.com>
2024-08-17 14:44:18 +0530

    getting logged in user data in frontend

--- client/src/api-services/user-service.ts
+++ client/src/api-services/user-service.ts
@@ -8,4 +8,9 @@ export const registerUser = async (data: any) => {
 export const loginUser = async (data: any) => {
   const response = await axios.post("/api/users/login", data);
   return response.data;
-}
+};
+
+export const getCurrentUser = async () => {
+  const response = await axios.get("/api/users/current-user");
+  return response.data;
+};
--- client/src/pages/private/home/home.tsx
+++ client/src/pages/private/home/home.tsx
@@ -1,5 +1,28 @@
+import { message } from "antd";
+import { useEffect, useState } from "react";
+import { getCurrentUser } from "../../../api-services/user-service";
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  const [user, setUser] = useState<any>(null);
+
+  const getData = async () => {
+    try {
+      const response = await getCurrentUser();
+      setUser(response.data);
+    } catch (error: any) {
+      message.error(error.response.data.message || error.message);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+  return (
+    <div className="p-5">
+      <h1>HomePage</h1>
+      <p>Welcome, {user?.name}</p>
+    </div>
+  );
 };
 
 export default HomePage;
