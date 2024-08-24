bac885f93073f9968160816c548d7628277464ad
Namit Malasi <namitmalasi@gmail.com>
2024-08-24 22:04:56 +0530

    created form for the general event details

--- client/src/pages/private/admin/events/common/event-form/event-form.tsx
+++ client/src/pages/private/admin/events/common/event-form/event-form.tsx
@@ -3,14 +3,29 @@ import General from "./general";
 import LocationAndDate from "./location-and-date";
 import Media from "./media";
 import Tickets from "./tickets";
-import { Steps } from "antd";
+import { Form, Steps } from "antd";
+
+export interface EventFormStepsProps {
+  eventData: any;
+  setEventData: any;
+  currentStep: number;
+  setCurrentStep: any;
+}
 
 const EventForm = () => {
   const [currentStep, setCurrentStep] = useState(0);
+  const [eventData, setEventData] = useState({});
+
+  const commonProps = {
+    eventData,
+    setEventData,
+    currentStep,
+    setCurrentStep,
+  };
   const stepsData = [
     {
       name: "General",
-      component: <General />,
+      component: <General {...commonProps} />,
     },
     {
       name: "Location And Date",
@@ -26,7 +41,7 @@ const EventForm = () => {
     },
   ];
   return (
-    <div>
+    <Form layout="vertical">
       <Steps current={currentStep} onChange={(step) => setCurrentStep(step)}>
         {stepsData.map((step, index) => (
           <Steps.Step key={index} title={step.name} />
@@ -34,7 +49,7 @@ const EventForm = () => {
       </Steps>
 
       <div className="mt-5">{stepsData[currentStep].component}</div>
-    </div>
+    </Form>
   );
 };
 
--- client/src/pages/private/admin/events/common/event-form/general.tsx
+++ client/src/pages/private/admin/events/common/event-form/general.tsx
@@ -1,5 +1,81 @@
-const General = () => {
-  return <div>General</div>;
+import { Button, Form, Input, Tag } from "antd";
+import { EventFormStepsProps } from "./event-form";
+import { useState } from "react";
+
+const General = ({
+  currentStep,
+  setCurrentStep,
+  eventData,
+  setEventData,
+}: EventFormStepsProps) => {
+  const [guestInputValue, setGuestInputValue] = useState("");
+
+  const onGuestAdd = () => {
+    const existingGuests = eventData.guests || [];
+    const newGuests = guestInputValue.split(",").map((guest) => guest.trim());
+    setEventData({ ...eventData, guests: [...existingGuests, ...newGuests] });
+    setGuestInputValue("");
+  };
+
+  const onGuestRemove = (index: number) => {
+    const existingGuests = eventData.guests || [];
+    const newGuests = existingGuests.filter(
+      (guest: string, i: number) => i !== index
+    );
+    setEventData({ ...eventData, guests: newGuests });
+  };
+  return (
+    <div className="flex flex-col gap-5">
+      <Form.Item label="Event Name" required>
+        <Input
+          placeholder="Event Name"
+          value={eventData.name}
+          onChange={(e) => setEventData({ ...eventData, name: e.target.value })}
+        />
+      </Form.Item>
+
+      <Form.Item label="Description" required>
+        <Input.TextArea
+          placeholder="Description"
+          value={eventData.description}
+          onChange={(e) =>
+            setEventData({ ...eventData, description: e.target.value })
+          }
+        />
+      </Form.Item>
+
+      <Form.Item label="Organizer" required>
+        <Input
+          placeholder="Organizer"
+          value={eventData.organizer}
+          onChange={(e) =>
+            setEventData({ ...eventData, organizer: e.target.value })
+          }
+        />
+      </Form.Item>
+
+      <Form.Item label="Guests List (comma seperated)" required>
+        <div className="flex gap-5">
+          <Input
+            placeholder="Guests List (comma seperated)"
+            value={guestInputValue}
+            onChange={(e) => setGuestInputValue(e.target.value)}
+          />
+          <Button onClick={onGuestAdd} disabled={!guestInputValue}>
+            Add
+          </Button>
+        </div>
+      </Form.Item>
+
+      <div className="flex flex-wrap gap-5">
+        {eventData.guests?.map((guest: string, index: number) => (
+          <Tag key={guest} closable onClose={() => onGuestRemove(index)}>
+            {guest}
+          </Tag>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default General;
