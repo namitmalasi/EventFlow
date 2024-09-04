1e58e218f449800c32fef735a49ed0087a773768
Namit Malasi <namitmalasi@gmail.com>
2024-09-04 14:54:30 +0530

    displaying events with details on homepage

--- client/src/pages/private/home/common/event-card.tsx
+++ client/src/pages/private/home/common/event-card.tsx
@@ -1,7 +1,48 @@
+import { Button } from "antd";
 import { EventType } from "../../../../interfaces";
+import { MapPin, Timer } from "lucide-react";
+import { getDateFormat } from "../../../../helpers/date-time-format";
 
 const EventCard = ({ event }: { event: EventType }) => {
-  return <div>{event.name}</div>;
+  const mainImage = event.media[0];
+  return (
+    <div className="grid lg:grid-cols-3 grid-cols-1 border border-solid border-gray-200 items-center">
+      <div className="col-span-1">
+        <img
+          src={mainImage}
+          alt={event.name}
+          className="w-full h-56 object-cover rounded-l"
+        />
+      </div>
+      <div className="col-span-2 flex flex-col gap-3 p-3">
+        <h1 className="text-primary text-sm font-bold">{event.name}</h1>
+        <p className="text-gray-600 text-sm line-clamp-3">
+          {event.description}
+        </p>
+
+        <div className="flex justify-between items-center">
+          <div className="bg-gray-200 p-2 rounded flex flex-col gap-1">
+            <div className="flex gap-2 items-center">
+              <MapPin size={16} />
+              <p className="text-xs">
+                {" "}
+                {event.address}, {event.city} {event.pincode}
+              </p>
+            </div>
+
+            <div className="flex gap-2 items-center">
+              <Timer size={16} />
+              <p className="text-xs">
+                {getDateFormat(`${event.date} ${event.time}`)}
+              </p>
+            </div>
+          </div>
+
+          <Button type="primary">View Details</Button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default EventCard;
